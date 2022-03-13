57702e44f2091a7da59caf1b5d8fe53a108ed825
Alaene Rufino de Sousa <alaenedesousa@gmail.com>
2022-03-13 18:16:16 -0300

    Update alaene-index.js

--- alaene-index.js
+++ alaene-index.js
@@ -3,12 +3,10 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Alaene Rufino de Sousa');
+app.get("/", function(req, res){
+    res.send('Alaene Rufino de Sousa');
 });
 
-server.listen(port, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+  console.info(`Server running at http://localhost:${port}`);
 });
